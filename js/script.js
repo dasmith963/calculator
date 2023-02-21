09cc044762bdbb1c330c8bb2aa689cb679c30054
Devon <Devon.smith017@gmail.com>
2023-02-20 21:12:08 -0600

    Create a function to call an operation.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,13 @@
 const operations = {
-  add: (a,b) => a + b,
-  subtract: (a,b) => a - b,
-  multiply: (a,b) => a * b,
-  divide: (a,b) => b === 0 ? 'Error' : a / b
+  add: (a, b) => a + b,
+  subtract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => b === 0 ? 'Error' : a / b
 }
 
+function operate(num1, num2, operator) {
+  if (operator === '+') return operations.add(num1, num2);
+  if (operator === '-') return operations.subtract(num1, num2);
+  if (operator === 'x') return operations.multiply(num1, num2);
+  if (operator === '÷') return operations.divide(num1, num2);
+}
