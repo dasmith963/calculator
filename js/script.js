b3e8185194ed3bd80c45b29d1920457d51ade6d8
Devon <Devon.smith017@gmail.com>
2023-02-20 22:27:07 -0600

    Create a function to populate the display.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
+const mainDisplay = document.querySelector('.main-display');
+const numberBtns = document.querySelectorAll('.number');
+let currentNum = '';
+
+numberBtns.forEach(button => button.addEventListener('click', appendNumber))
+
 const operations = {
   add: (a, b) => a + b,
   subtract: (a, b) => a - b,
@@ -10,4 +16,11 @@ function operate(num1, num2, operator) {
   if (operator === '-') return operations.subtract(num1, num2);
   if (operator === 'x') return operations.multiply(num1, num2);
   if (operator === '÷') return operations.divide(num1, num2);
+}
+
+function appendNumber(e){
+  currentNum === '0' 
+    ? currentNum = e.target.textContent
+    : currentNum += e.target.textContent;
+  mainDisplay.textContent = currentNum;
 }
